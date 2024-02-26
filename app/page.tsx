b0080e7d3a2e5715234262be399e2d0a2ed560d5
Chathura-Nimsara <99wakcnk@gmail.com>
2024-02-26 09:38:09 +0530

    adding Card to Components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import ClientForm from '@/components/ClientForm';
+import Card from '@/components/Card';
+
 import { lucia, validateRequest } from '@/modules/auth/lucia';
 
 export default async function Page() {
@@ -17,6 +19,11 @@ export default async function Page() {
         <ClientForm action={logout} initialState={{ error: '' }}>
           <button>Sign out</button>
         </ClientForm>
+        <Card title="AI-Driven Emissions Intelligence "
+              content="Harness the power of AI for precise emissions calculations and recommendations, optimizing your climate mitigation strategies." 
+              image="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'%3E%3Cpath d='M19 21H5C4.44772 21 4 20.5523 4 20V11L1 11L11.3273 1.6115C11.7087 1.26475 12.2913 1.26475 12.6727 1.6115L23 11L20 11V20C20 20.5523 19.5523 21 19 21ZM13 19H18V9.15745L12 3.7029L6 9.15745V19H11V13H13V19Z'%3E%3C/path%3E%3C/svg%3E" 
+              button={<button></button>}
+        ></Card>
       </div>
     </div>
   );
---
+++ components/Card.tsx
@@ -0,0 +1,35 @@
+// Card.js
+import React from 'react';
+import Image from 'next/image';
+
+interface CardProps {
+  title: string;
+  content: string;
+  image: string;
+  button: JSX.Element;
+}
+
+const Card: React.FC<CardProps> = ({ title, content,image,button }) => {
+  return (
+    <div className="rounded-3xl border-4 border-black border-solid  p-4 h-full w-80 bg-gray-50">
+        
+      <div className="relative rounded-full overflow-hidden  h-20 w-20 bg-teal-400">
+        <Image src={image} alt={title} layout="fill" objectFit="cover" className="rounded-t-xl p-2" />
+      </div>
+      <div className="p-4">
+        <h1 className="text-xl font-bold mb-2">{title}</h1>
+        <p className="text-slate-400 subpixel-antialiased mb-4">{content}</p>
+        <div className="flex justify-end">
+        <a href="https://www.figma.com/file/WoirximBPom3gqLmFhpoxK/SusDev-OS?type=design&node-id=323-813&mode=dev" 
+            className="bg-black /*hover:bg-blue-700*/ h-14 w-14  flex items-center justify-center rounded-full">
+            <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='36' height='36' fill='rgba(255,255,255,1)'%3E%3Cpath fill='none' d='M0 0h24v24H0z'%3E%3C/path%3E%3Cpath d='M16.0037 9.41421L7.39712 18.0208L5.98291 16.6066L14.5895 8H7.00373V6H18.0037V17H16.0037V9.41421Z'%3E%3C/path%3E%3C/svg%3E" 
+            alt="Button Image" className=" w-10 h-10  rounded-full" />
+        {button}
+        </a>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
